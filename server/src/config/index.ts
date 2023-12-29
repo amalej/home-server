6a2855103df1cd31c66b1bb6152813cb240d7506
amalej <alejandroalejandre15@gmail.com>
2023-12-30 05:44:31 +0800

    Updated server configs

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -5,10 +5,10 @@ export const serverEnv = process.env.SERVER_ENVIRONMENT || "development";
 export const isProd = serverEnv === "production";
 
 export const webClientPath = isProd
-  ? path.join(__dirname, "..", `${process.env.WEB_CLIENT_BUILD_DIR}`)
+  ? path.join(__dirname, "..", "..", `${process.env.WEB_CLIENT_BUILD_DIR}`)
   : path.join(__dirname, "..", "..", "..", "web-client", "build");
 export const uploadPath = isProd
-  ? path.join(__dirname, "..", "uploads/")
+  ? path.join(__dirname, "..", "..", "uploads/")
   : path.join(__dirname, "..", "..", "..", "uploads/");
 
 export const hostAddress = findHostAddress();
